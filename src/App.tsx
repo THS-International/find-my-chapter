5c72909b3d13ab5eee388fd2a71f5e25c0418dbb
Dylan Kreiner <dylanhornkreiner@gmail.com>
2025-06-28 11:41:26 +0200

    stuff

--- src/App.tsx
+++ src/App.tsx
@@ -19,10 +19,19 @@ function App() {
 					className="mx-auto w-48 sm:w-75 h-auto"
 				/>
 				<p className="text-center text-gray-700 dark:text-gray-200 mt-2 mb-4 w-full px-4">
-					Created by Dylan Horn Kreiner in 2025. This site is still under development (clearly lol) for any urgent issues (like your
-					programme not being listed), please contact{" "}
+					Created by Dylan Horn Kreiner in 2025. This site is still under
+					development (clearly lol) for any urgent issues (like your programme
+					not being listed), please contact{" "}
 					<a href="mailto:it@thsint.se" className="underline">
 						it@thsint.se
+					</a>
+					. All information is sourced from{" "}
+					<a
+						href="https://docs.google.com/spreadsheets/d/e/2PACX-1vRBIcNbmG1vW0L47mWMuLZHwui7QHFuIz8RlkTrKtgIUh21nwcyDRdkFi56i1O2j1EvkmFlEqJ4yw0i/pubhtml"
+						className="underline"
+					>
+						{" "}
+						this spreadsheet
 					</a>
 				</p>
 			</footer>
